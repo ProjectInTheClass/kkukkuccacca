6ea03e11d85c9ab0dbd4e164d1cfdfeb2a0a50ae
andykim14 <andykim14@naver.com>
2018-07-25 16:19:00 +0900

    func

--- foodlesummer/foodlesummer/ViewController.swift
+++ foodlesummer/foodlesummer/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func giveme() {
+        print("hihihi")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,6 +21,9 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+    
+        ////////////
         // Dispose of any resources that can be recreated.
     }
 
