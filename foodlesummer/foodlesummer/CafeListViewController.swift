ac4ce664dc8e412766c4255eb3a5bd723d70ce73
andykim14 <andykim14@naver.com>
2018-08-08 15:27:17 +0900

    카페6~10 주소수정

--- foodlesummer/foodlesummer/CafeListViewController.swift
+++ foodlesummer/foodlesummer/CafeListViewController.swift
@@ -24,15 +24,15 @@ class CafeListViewController: UIViewController,UITableViewDataSource, UITableVie
         Cafe (cafeNames: "미엘",cafeImages: "image_cafe1",
               cafeHour: "매일 11:00 - 24:00",cafeAddress: "서울 강남구 도산대로67길 13-12", cafeWifi : true, cafeToilet : true, cafeSmoking : false, cafeParking : false, rate : 5.0),
         Cafe (cafeNames: "cafe_the_healing",cafeImages: "image_cafe6",
-              cafeHour: "09:00 ~ 21:00",cafeAddress: "서울", cafeWifi : false, cafeToilet : false, cafeSmoking : true, cafeParking : true, rate : 5.0),
+              cafeHour: "09:00 ~ 21:00",cafeAddress: "서울 광진구 능동로11길 10", cafeWifi : false, cafeToilet : false, cafeSmoking : true, cafeParking : true, rate : 5.0),
         Cafe (cafeNames: "Less is More",cafeImages: "image_cafe7",
-              cafeHour: "08:00 ~ 22:00",cafeAddress: "서울", cafeWifi : true, cafeToilet : false, cafeSmoking : false, cafeParking : true, rate : 5.0),
+              cafeHour: "08:00 ~ 22:00",cafeAddress: "서울 광진구 동일로20길 89", cafeWifi : true, cafeToilet : false, cafeSmoking : false, cafeParking : true, rate : 5.0),
         Cafe (cafeNames: "103 express",cafeImages: "image_cafe8",
-              cafeHour: "08:00 ~ 18:00",cafeAddress: "서울", cafeWifi : false, cafeToilet : false, cafeSmoking : false, cafeParking : false, rate : 5.0),
+              cafeHour: "08:00 ~ 18:00",cafeAddress: "서울 광진구 능동로13길 23", cafeWifi : false, cafeToilet : false, cafeSmoking : false, cafeParking : false, rate : 5.0),
         Cafe (cafeNames: "Cafe_be_Happy",cafeImages: "image_cafe9",
-              cafeHour: "09:00 ~ 23:00",cafeAddress: "서울", cafeWifi : false, cafeToilet : false, cafeSmoking : true, cafeParking : false, rate : 5.0),
+              cafeHour: "09:00 ~ 23:00",cafeAddress: "서울 광진구 동일로22길", cafeWifi : false, cafeToilet : false, cafeSmoking : true, cafeParking : false, rate : 5.0),
         Cafe (cafeNames: "Silverlick",cafeImages: "image_cafe10",
-              cafeHour: "08:00 ~ 24:00",cafeAddress: "서울", cafeWifi : false, cafeToilet : false, cafeSmoking : false, cafeParking : false, rate : 5.0)
+              cafeHour: "08:00 ~ 24:00",cafeAddress: "서울 광진구 능동로50길 9", cafeWifi : false, cafeToilet : false, cafeSmoking : false, cafeParking : false, rate : 5.0)
         
         ]
     
